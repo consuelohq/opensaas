9325ca4c2ad21bfe669e889ebe3e587ce83a3808
kokayi <kokayi@consuelohq.com>
2026-03-19 17:24:11 -0400

    fix(twenty-server): support graphql in workspace auth guard

--- packages/twenty-server/src/engine/guards/workspace-auth.guard.ts
+++ packages/twenty-server/src/engine/guards/workspace-auth.guard.ts
@@ -1,4 +1,5 @@
 import { type CanActivate, type ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 import { type Observable } from 'rxjs';
 
@@ -6,7 +7,10 @@ export class WorkspaceAuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request =
+      context.getType<string>() === 'http'
+        ? context.switchToHttp().getRequest()
+        : GqlExecutionContext.create(context).getContext().req;
 
     return request.workspace !== undefined;
   }
